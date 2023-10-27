ca802104bb6f4363a9acfd4c97d20b887229db3a
iostream-code <aryakmifjannah2603@gmail.com>
2023-10-27 18:29:14 +0700

    create view react js

---
+++ react-js-crud/src/views/home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div className="p-5 mb-4 bg-light rounded-3">
+            <div className="container-fluid py-5">
+                <h1 className="display-5 fw-bold">React (Vite) + Laravel 10</h1>
+                <p className="col-md-8 fs-4">Learning how to use Api from Laravel in React Js</p>
+            </div>
+        </div>
+    )
+}
